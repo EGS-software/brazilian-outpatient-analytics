1848c1ace09a2402233eb47ac5227bd33b4a539c
JaoVicy <filipimjoa@gmail.com>
2026-06-26 16:29:26 -0300

    style: formating analytics.py defs logs

--- service/analytics.py
+++ service/analytics.py
@@ -5,21 +5,21 @@ import config
 from mlxtend.frequent_patterns import apriori, association_rules
 
 def minerar_regras(df_analise):
-    print("📊 [Analytics] Convertendo dados para formato binário (One-Hot)...")
+    print("[Analytics] Convertendo dados para formato binário (One-Hot)...")
     df_binario = pd.get_dummies(df_analise)
     
-    print(f"🚀 [Analytics] Executando Apriori (Suporte Min: {config.SUPORTE_MIN})...")
+    print(f"[Analytics] Executando Apriori (Suporte Min: {config.SUPORTE_MIN})...")
     itens_frequentes = apriori(df_binario, min_support=config.SUPORTE_MIN, use_colnames=True)
     
     if itens_frequentes.empty:
-        print("⚠️ [Analytics] Nenhum item frequente encontrado com este suporte.")
+        print("[Analytics] Nenhum item frequente encontrado com este suporte.")
         return None
         
-    print(f"⚖️ [Analytics] Gerando regras de associação (Confiança Min: {config.CONFIANCA_MIN})...")
+    print(f"[Analytics] Gerando regras de associação (Confiança Min: {config.CONFIANCA_MIN})...")
     regras = association_rules(itens_frequentes, metric="confidence", min_threshold=config.CONFIANCA_MIN)
     
     if regras.empty:
-        print("⚠️ [Analytics] Nenhuma regra gerada com esta confiança.")
+        print("[Analytics] Nenhuma regra gerada com esta confiança.")
         return None
         
     # Formatação amigável para leitura humana
@@ -27,14 +27,14 @@ def minerar_regras(df_analise):
     regras['consequents'] = regras['consequents'].apply(lambda x: ', '.join(list(x)))
     regras = regras.sort_values(by='lift', ascending=False)
     
-    print(f"✔️ [Analytics] {len(regras)} regras criadas com sucesso.")
+    print(f"[Analytics] {len(regras)} regras criadas com sucesso.")
     return regras
 
 def plotar_resultados(regras):
     if regras is None or regras.empty:
         return
         
-    print("📉 [Analytics] Gerando e salvando gráfico de dispersão...")
+    print("[Analytics] Gerando e salvando gráfico de dispersão...")
     plt.figure(figsize=(10, 6))
     scatter = plt.scatter(
         regras['support'], regras['confidence'], 
